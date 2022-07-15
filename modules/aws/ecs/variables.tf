4e20c9c33c8dfa093ed4464370d25d68c5d87133
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-15 11:08:25 -0700

    Update variables.tf

--- modules/aws/ecs/variables.tf
+++ modules/aws/ecs/variables.tf
@@ -8,11 +8,6 @@ variable "tags" {
   type        = map(string)
 }
 
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID."
-}
-
 variable "alb_sg" {
   description = "The security groups to attach to the load balancer. e.g. [\"sg-edcd9784\",\"sg-edcd9785\"]"
   type        = list(string)
