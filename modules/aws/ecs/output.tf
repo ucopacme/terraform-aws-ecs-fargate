2fb1db85faa8234b007aad8d60fc8ad1be15d5d3
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-15 12:56:21 -0700

    Update output.tf

--- modules/aws/ecs/output.tf
+++ modules/aws/ecs/output.tf
@@ -12,3 +12,8 @@ output "cluster_arn" {
   description = "The ARN of the created ECS cluster."
   value       = aws_ecs_cluster.this.arn
 }
+
+output "iam_role_arn" {
+  description = "ARN of IAM role"
+  value       = aws_iam_role.this.arn
+}
