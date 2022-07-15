cdee36f457c07e6569b9fb0b964ec491fc6dd989
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-15 12:28:39 -0700

    Update variables.tf

--- modules/aws/standard-ecs-cluster/variables.tf
+++ modules/aws/standard-ecs-cluster/variables.tf
@@ -18,6 +18,21 @@ variable "region" {
   default = "us-west-2"
 }
 
+variable "image" {
+  description = "Prefix (eg. abc) of the instance profile (abcProfile) and role (abcRole) names"
+  type        = "string"
+}
+
+variable "containerport" {
+  description = "Prefix (eg. abc) of the instance profile (abcProfile) and role (abcRole) names"
+  type        = "string"
+}
+
+variable "hostport" {
+  description = "Prefix (eg. abc) of the instance profile (abcProfile) and role (abcRole) names"
+  type        = "string"
+}
+
 provider "aws" {
   access_key = ""
   secret_key = ""
