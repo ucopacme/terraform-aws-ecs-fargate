d828b75c6fe552f3a537cceea51e9011cf6ad78c
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-15 17:26:44 -0700

    Update main.tf

--- modules/aws/standard-ecs-cluster/main.tf
+++ modules/aws/standard-ecs-cluster/main.tf
@@ -9,6 +9,7 @@ module "ecs-task-def" {
     containerport = 80
     hostport = 80
     image = var.image
-    execution_role_arn = module.ecs.execution_role_arn
+    #execution_role_arn = module.ecs.execution_role_arn
+    execution_role_arn = "arn:aws:iam::944706592399:role/ecs-kk-test-ecs_ecsTaskExecutionRole"
 }
 
