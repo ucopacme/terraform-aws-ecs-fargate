48e0074993c2a2b7f189adb50d5aed480a64e127
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-15 17:56:01 -0700

    Update ecs-task-def.tf

--- modules/aws/ecs-task-def/ecs-task-def.tf
+++ modules/aws/ecs-task-def/ecs-task-def.tf
@@ -21,6 +21,5 @@ resource "aws_ecs_task_definition" "this" {
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn                   = 256         # Specifying the CPU our container requires
   execution_role_arn       = var.execution_role_arn
 }
