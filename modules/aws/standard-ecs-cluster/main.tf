f64252f012aa7bb8fec2fa1ef3e21c5c53f2c24d
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-15 17:22:56 -0700

    Update main.tf

--- modules/aws/standard-ecs-cluster/main.tf
+++ modules/aws/standard-ecs-cluster/main.tf
@@ -2,6 +2,13 @@ module "ecs"{
     source = "../ecs"
     name = join("-", [var.name, "ecs"])
     tags = merge(var.tags, map("Name", var.name))
+}
 
+    module "ecs-task-def" {
+    source = "../ecs-task-def"
+    containerport = 80
+    hostport = 80
+    image = var.image
+    execution_role_arn = module.ecs.execution_role_arn
 }
 
