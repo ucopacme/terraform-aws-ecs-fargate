8f577d6558b1b11fe3f6fea849d5cb24b9bdb585
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-18 10:35:41 -0700

    Update ecs.tf

--- modules/aws/ecs/ecs.tf
+++ modules/aws/ecs/ecs.tf
@@ -42,7 +42,7 @@ data "aws_iam_policy_document" "task_role" {
 
 resource "aws_iam_role" "execution_role" {
   name               = "${var.name}_ecsTaskExecutionRole"
-  assume_role_policy = "data.aws_iam_policy_document.assume_by_ecs.json"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_by_ecs.json}"
 }
 
 resource "aws_iam_role_policy" "execution_role" {
@@ -52,12 +52,12 @@ resource "aws_iam_role_policy" "execution_role" {
 
 resource "aws_iam_role" "task_role" {
   name               = "${var.name}_ecsTaskRole"
-  assume_role_policy = "data.aws_iam_policy_document.assume_by_ecs.json"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_by_ecs.json}"
 }
 
 resource "aws_iam_role_policy" "task_role" {
   role   = "aws_iam_role.task_role.name"
-  policy = "data.aws_iam_policy_document.task_role.json"
+  policy = "${data.aws_iam_policy_document.task_role.json}"
 }
 
 resource "aws_ecs_cluster" "this" {
