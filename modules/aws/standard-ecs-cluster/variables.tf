4b07561ab9d868a141c7a5344206d0120fc6e686
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-18 10:44:05 -0700

    Update variables.tf

--- modules/aws/standard-ecs-cluster/variables.tf
+++ modules/aws/standard-ecs-cluster/variables.tf
@@ -40,15 +40,7 @@ variable "cpu" {
   description = "task df cpu"
   type        = string
 }
-provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "${var.region}"
-}
 
-variable "ecs_key_pair_name" {
-  default = ""
-}
 
 variable "aws_account_id" {
   default = ""
