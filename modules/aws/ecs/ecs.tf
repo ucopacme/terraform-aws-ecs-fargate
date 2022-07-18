d157f30500418ea937348264d67a141e4342fde0
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-18 10:37:42 -0700

    Update ecs.tf

--- modules/aws/ecs/ecs.tf
+++ modules/aws/ecs/ecs.tf
@@ -42,7 +42,7 @@ data "aws_iam_policy_document" "task_role" {
 
 resource "aws_iam_role" "execution_role" {
   name               = "${var.name}_ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_by_ecs.json}"
+  assume_role_policy = "data.aws_iam_policy_document.assume_by_ecs.json"
 }
 
 resource "aws_iam_role_policy" "execution_role" {
