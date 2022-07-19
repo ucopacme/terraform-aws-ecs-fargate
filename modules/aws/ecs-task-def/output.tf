dbe935ba17cfe54b778cb2b2f50b2f09de59141d
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-19 16:55:00 -0700

    Update output.tf

--- modules/aws/ecs-task-def/output.tf
+++ modules/aws/ecs-task-def/output.tf
@@ -7,8 +7,3 @@ output "revision" {
   description = "The revision of the task in a particular family"
   value       = join("", aws_ecs_task_definition.this.*.revision)
 }
-
-output "target_group_arns" {
-  description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
-  value       = module.alb.target_group_arns
-}
