e533b86e12f3de358d0776f358cb9c015c371b7f
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-18 17:20:32 -0700

    Update main.tf

--- modules/aws/standard-ecs-cluster/main.tf
+++ modules/aws/standard-ecs-cluster/main.tf
@@ -16,50 +16,5 @@ module "ecs-task-def" {
     execution_role_arn = module.ecs.execution_role_arn
 }
 
-module "alb" {
-  source = "git::https://git@github.com/ucopacme/terraform-aws-alb-nlb//?ref=v0.0.5"
-  name   = var.name
-#   enabled            = "true"
-#   load_balancer_type = "application"
-   vpc_id             = var.vpc_id
-   subnets            = var.subnets
-   security_groups    = var.security_groups
-     target_groups = [
-    
-    {
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "ip"
-   
-       health_check = {
-        matcher = "/"
-      }
-     
-    },
-     
-    {
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "ip"
-    #   # Add instances to target groups
-    },
-  ]
-   
-  
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-      action_type = "redirect"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    },
-  ]
 
- 
-}
   
