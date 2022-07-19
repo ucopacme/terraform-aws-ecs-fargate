de91c75b1de054dfcf3862aa4e5126554516ebed
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-18 18:38:42 -0700

    Update outputs.tf

--- modules/aws/standard-ecs-cluster/outputs.tf
+++ modules/aws/standard-ecs-cluster/outputs.tf
@@ -22,3 +22,8 @@ output "family" {
   description = "The family of your task definition, used as the definition name"
   value       = module.ecs-task-def.family
 }
+    
+    output "revision" {
+  description = "The revision of the task in a particular family"
+  value       = join("", aws_ecs_task_definition.this.*.revision)
+}
