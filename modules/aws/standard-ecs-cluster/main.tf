70692cd61bf4b0e2afe2484dc7b5078e778e7407
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-20 10:24:28 -0700

    Update main.tf

--- modules/aws/standard-ecs-cluster/main.tf
+++ modules/aws/standard-ecs-cluster/main.tf
@@ -1,58 +1,4 @@
-module "alb" {
-  name               = var.name
-  source             = "../alb"
-  enabled            = "true"
-  load_balancer_type = "application"
-  vpc_id             = var.vpc_id
-  subnets            = var.subnets
-  security_groups    = ["sg-032b4eb81e054cc1f"]
 
-  target_groups = [
-
-    {
-      backend_protocol = "${var.backend_protocol}"
-      backend_port     = "${var.backend_port}"
-      target_type      = "ip"
-      vpc_id           = var.vpc_id
-
-    },
-
-    {
-      backend_protocol = "${var.backend_protocol}"
-      backend_port     = "${var.backend_protocol}"
-      target_type      = "ip"
-      vpc_id           = var.vpc_id
-      #   # Add instances to target groups
-    },
-  ]
-  # Create HTTPs listners
-  # https_listeners = [
-  #   {
-  #     port     = 80
-  #     protocol = "HTTP"
-  #     # certificate_arn    = "arn:aws:acm:us-west-2:897194160541:certificate/87a248af-588b-4aa4-8463-820febd286b4"
-  #     target_group_index = 0
-  #     # ssl_policy         = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
-  #   }
-  # ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-      # action_type        = "redirect"
-      # redirect = {
-      #   port        = "443"
-      #   protocol    = "HTTPS"
-      #   status_code = "HTTP_301"
-      # }
-    },
-  ]
- tags = var.tags
-
-}
-  
   
 module "ecs"{
     source = "../ecs"
@@ -72,9 +18,9 @@ module "ecs_task_def" {
     #execution_role_arn = module.ecs.execution_role_arn
     execution_role_arn = module.ecs.execution_role_arn
     cluster = module.ecs.cluster_arn
-    target_group_arn = module.alb.target_group_arns[0]
+    target_group_arn = var.target_group_arn
     #depends_on = module.alb.http_tcp_listener_arns
-    depends_on = [module.alb.target_group_arns]
+    #depends_on = [module.alb.target_group_arns]
     
 }
 
