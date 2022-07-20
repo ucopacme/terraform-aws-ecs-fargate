ea4e8701832505646ce31d9515f2bfeb76dd2b14
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-20 13:25:10 -0700

    Update ecs-task-def.tf

--- modules/aws/ecs-task-services/ecs-task-def.tf
+++ modules/aws/ecs-task-services/ecs-task-def.tf
@@ -76,7 +76,7 @@ resource "aws_ecs_service" "this" {
 
 
 resource "aws_appautoscaling_target" "target" {
-  service_namespace  = join("-", [var.name, "service"])
+  service_namespace  = join("-", [var.name, "autoscale"])
   resource_id        = "service/var.cluster/${aws_ecs_service.this.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   role_arn           = "arn:aws:iam::944706592399:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling_kk-test"
