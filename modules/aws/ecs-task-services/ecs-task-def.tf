4c5d4ebf38b49e1f9a03b8b81e42581e799f01e8
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-20 13:12:45 -0700

    Update ecs-task-def.tf

--- modules/aws/ecs-task-services/ecs-task-def.tf
+++ modules/aws/ecs-task-services/ecs-task-def.tf
@@ -71,6 +71,99 @@ resource "aws_ecs_service" "this" {
   #depends_on = [var.http_tcp_listener_arns]
 }
 
+
+## Autoscaling
+
+
+resource "aws_appautoscaling_target" "target" {
+  service_namespace  = join("-", [var.name, "service"])
+  resource_id        = "service/var.cluster/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  role_arn           = "arn:aws:iam::944706592399:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling_kk-test"
+  min_capacity       = 1
+  max_capacity       = 4
+}
+
+# Automatically scale capacity up by one
+resource "aws_appautoscaling_policy" "up" {
+  name               = "cb_scale_up"
+  service_namespace  = "ecs"
+  resource_id        = "service/var.cluster/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+
+  depends_on = [aws_appautoscaling_target.target]
+}
+
+# Automatically scale capacity down by one
+resource "aws_appautoscaling_policy" "down" {
+  name               = "cb_scale_down"
+  service_namespace  = "ecs"
+  resource_id        = "service/var.cluster/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+
+  depends_on = [aws_appautoscaling_target.target]
+}
+
+# CloudWatch alarm that triggers the autoscaling up policy
+resource "aws_cloudwatch_metric_alarm" "service_cpu_high" {
+  alarm_name          = "cb_cpu_utilization_high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "40"
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.this.name
+    ServiceName = aws_ecs_service.this.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.up.arn]
+}
+
+# CloudWatch alarm that triggers the autoscaling down policy
+resource "aws_cloudwatch_metric_alarm" "service_cpu_low" {
+  alarm_name          = "cb_cpu_utilization_low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "10"
+
+  dimensions = {
+    ClusterName = var.cluster
+    ServiceName = aws_ecs_service.this.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.down.arn]
+}
+
 # resource "aws_security_group" "service_security_group" {
 #   name = "kk-ecs-test"
 #   vpc_id      = "vpc-06a0bfef01b9d0e7b"
