10964e289cb2198426f4c3005b26fa58ffd2b1bd
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-19 17:07:53 -0700

    Update main.tf

--- modules/aws/standard-ecs-cluster/main.tf
+++ modules/aws/standard-ecs-cluster/main.tf
@@ -22,7 +22,7 @@ module "ecs_task_def" {
     #execution_role_arn = module.ecs.execution_role_arn
     execution_role_arn = module.ecs.execution_role_arn
     cluster = module.ecs.cluster_arn
-    target_group_arn = module.alb.target_group_arns[0]
+    target_group_arn = [module.alb.target_group_arns[0]]
     #depends_on = module.alb.http_tcp_listener_arns
     depends_on = [module.alb.target_group_arns]
     
