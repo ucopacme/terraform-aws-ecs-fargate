304a965bf6afd1b0b8a408a569ef7cc707d462bf
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-19 17:12:31 -0700

    Update main.tf

--- modules/aws/standard-ecs-cluster/main.tf
+++ modules/aws/standard-ecs-cluster/main.tf
@@ -1,6 +1,64 @@
 module "alb" {
-source = "../alb"
-name = var.name
+  source        = "../ecs"
+  name          = join("-", [local.application, local.environment])
+  tags = {
+    "ucop:application" = local.application
+    "ucop:createdBy"   = local.createdBy
+    "ucop:environment" = local.environment
+    "ucop:group"       = local.group
+    "ucop:source"      = local.source
+  }
+  enabled            = "true"
+  load_balancer_type = "application"
+  vpc_id             = var.vpc_id
+  subnets            = var.subnets
+  security_groups    = ["sg-032b4eb81e054cc1f"]
+
+  target_groups = [
+
+    {
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "ip"
+      vpc_id           = var.vpc_id
+
+    },
+
+    {
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "ip"
+      vpc_id           = var.vpc_id
+      #   # Add instances to target groups
+    },
+  ]
+  # Create HTTPs listners
+  # https_listeners = [
+  #   {
+  #     port     = 80
+  #     protocol = "HTTP"
+  #     # certificate_arn    = "arn:aws:acm:us-west-2:897194160541:certificate/87a248af-588b-4aa4-8463-820febd286b4"
+  #     target_group_index = 0
+  #     # ssl_policy         = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
+  #   }
+  # ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+      # action_type        = "redirect"
+      # redirect = {
+      #   port        = "443"
+      #   protocol    = "HTTPS"
+      #   status_code = "HTTP_301"
+      # }
+    },
+  ]
+
+
+}
 
 
 }
@@ -22,7 +80,7 @@ module "ecs_task_def" {
     #execution_role_arn = module.ecs.execution_role_arn
     execution_role_arn = module.ecs.execution_role_arn
     cluster = module.ecs.cluster_arn
-    target_group_arn = [module.alb.target_group_arns[0]]
+    target_group_arn = module.alb.target_group_arns[0]
     #depends_on = module.alb.http_tcp_listener_arns
     depends_on = [module.alb.target_group_arns]
     
