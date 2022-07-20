a3882f65689c2c6e8ea5e3e604a49152e4bc1230
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-07-20 13:45:48 -0700

    Update ecs-task-def.tf

--- modules/aws/ecs-task-services/ecs-task-def.tf
+++ modules/aws/ecs-task-services/ecs-task-def.tf
@@ -71,6 +71,20 @@ resource "aws_ecs_service" "this" {
   #depends_on = [var.http_tcp_listener_arns]
 }
 
+# Set up CloudWatch group and log stream and retain logs for 30 days
+resource "aws_cloudwatch_log_group" "cb_log_group" {
+  name              = "/ecs/cb-app"
+  retention_in_days = 30
+
+  tags = {
+    Name = "cb-log-group"
+  }
+}
+
+resource "aws_cloudwatch_log_stream" "cb_log_stream" {
+  name           = "cb-log-stream"
+  log_group_name = aws_cloudwatch_log_group.cb_log_group.name
+}
 
 ## Autoscaling
 
