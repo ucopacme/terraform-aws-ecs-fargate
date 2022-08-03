b84976b95db9f44213aca99cc8669ddfa0bb2ae8
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2022-08-03 14:44:40 -0700

    .

--- modules/aws/ecs-task-services/output.tf
+++
@@ -1,9 +0,0 @@
-output "family" {
-  description = "The family of your task definition, used as the definition name"
-  value       = join("", aws_ecs_task_definition.this.*.family)
-}
-
-output "revision" {
-  description = "The revision of the task in a particular family"
-  value       = join("", aws_ecs_task_definition.this.*.revision)
-}
