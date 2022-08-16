a29ef94c181f7ce206bf304bed37bff9521110d7
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-08-16 13:04:23 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
   description = "The ID of the created ECS cluster."
-  value       = aws_ecs_cluster.this.id
+  value       = join("", aws_ecs_cluster.this.*.id)
 }
 
 output "cluster_name" {
   description = "The name of the created ECS cluster."
-  value       = aws_ecs_cluster.this.name
+  value       = join("", aws_ecs_cluster.this.*.name)
 }
 
 output "cluster_arn" {
   description = "The ARN of the created ECS cluster."
-  value       = aws_ecs_cluster.this.arn
+  value       = join("", aws_ecs_cluster.this.*.arn)
 }
 
 output "execution_role_arn" {
